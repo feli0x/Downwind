92eb340fe7ee2cb218f2d9d87aa1e8891d2e436d
Felix Swinkels <felix@code.nl>
2023-10-30 21:16:24 +0100

    add remove tailwind and all classes functions

--- src/extension.ts
+++ src/extension.ts
@@ -1,40 +1,114 @@
 import * as vscode from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
-    "extension.removeTailwindClasses",
-    () => {
-      const editor = vscode.window.activeTextEditor;
-      if (!editor) {
-        vscode.window.showErrorMessage("No active editor found");
-        return;
-      }
+// function to remove tailwind classes
+export function removeTailwindClasses(): void {
+  // get the active text editor
+  const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
+  // if no active editor found, show error message and return
+  if (!editor) {
+    vscode.window.showErrorMessage("No active editor found");
+    return;
+  }
 
-      const text = editor.document.getText();
-      const regex =
-        /(?:^|\s)(?:bg|text|border|divide|ring|ring-offset|shadow|opacity|mix-blend|filter|backdrop|blur|brightness|contrast|drop-shadow|grayscale|hue-rotate|invert|saturate|sepia|transition|duration|ease|delay)-[\w-]+/g;
-      const newText = text.replace(regex, "");
+  // list of tailwind classes to remove
+  const classList: string[] = [
+    "bg",
+    "text",
+    "leading",
+    "border",
+    "divide",
+    "ring",
+    "shadow",
+    "opacity",
+    "transition",
+    "duration",
+    "ease",
+    "delay",
+  ];
 
-      if (newText === text) {
-        vscode.window.showInformationMessage("No tailwind classes found");
-        return;
-      }
-
-      editor.edit((editBuilder) => {
-        editBuilder.replace(
-          new vscode.Range(
-            editor.document.positionAt(0),
-            editor.document.positionAt(text.length)
-          ),
-          newText
-        );
-      });
-
-      vscode.window.showInformationMessage(`Removed tailwind classes`);
-    }
+  // regex to match tailwind classes
+  const regex: RegExp = new RegExp(
+    `(?:^|\\s)(?:${classList.join("|")})-[\\w-]+`,
+    "g"
   );
 
-  context.subscriptions.push(disposable);
+  // get the current selection
+  const selection: vscode.Selection = editor.selection;
+  // get the text within the selection
+  const text: string = editor.document.getText(selection);
+  // remove tailwind classes from the text
+  const newText: string = text.replace(regex, "");
+
+  // if no tailwind classes found, show information message and return
+  if (newText === text) {
+    vscode.window.showInformationMessage("No tailwind classes found");
+    return;
+  }
+
+  // replace the text within the selection with the new text
+  editor.edit((editBuilder: vscode.TextEditorEdit) => {
+    editBuilder.replace(selection, newText);
+  });
+
+  // show information message
+  vscode.window.showInformationMessage(`Removed tailwind classes`);
 }
 
-export function deactivate() {}
+// function to remove all classes
+export function removeAllClasses(): void {
+  // get the active text editor
+  const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
+  // if no active editor found, show error message and return
+  if (!editor) {
+    vscode.window.showErrorMessage("No active editor found");
+    return;
+  }
+
+  // regex to match class attribute
+  const regex: RegExp = /class\s*=\s*"([^"]*)"/g;
+
+  // get the current selection
+  const selection: vscode.Selection = editor.selection;
+  // get the text within the selection
+  const text: string = editor.document.getText(selection);
+  // remove all classes from the text
+  const newText: string = text.replace(regex, 'class=""');
+
+  // if no classes found, show information message and return
+  if (newText === text) {
+    vscode.window.showInformationMessage("No classes found");
+    return;
+  }
+
+  // replace the text within the selection with the new text
+  editor.edit((editBuilder: vscode.TextEditorEdit) => {
+    editBuilder.replace(selection, newText);
+  });
+
+  // show information message
+  vscode.window.showInformationMessage(`Emptied all classes`);
+}
+
+// function to activate the extension
+export function activate(context: vscode.ExtensionContext): void {
+  // register the removeTailwindClasses command
+  let removeTailwindClassesFunction: vscode.Disposable =
+    vscode.commands.registerCommand(
+      "extension.removeTailwindClasses",
+      removeTailwindClasses
+    );
+
+  // register the removeAllClasses command
+  let removeAllClassesFunction: vscode.Disposable =
+    vscode.commands.registerCommand(
+      "extension.removeAllClasses",
+      removeAllClasses
+    );
+
+  // add the commands to the context subscriptions
+  context.subscriptions.push(removeTailwindClassesFunction);
+  context.subscriptions.push(removeAllClassesFunction);
+}
+
+// function to deactivate the extension
+export function deactivate(): void {}
