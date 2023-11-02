fdb60ee387712a7bb67c4e6c69d1a12c7518508d
Felix Swinkels <felix@code.nl>
2023-11-02 15:51:01 +0100

    categories for removing functions

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,89 @@
 import * as vscode from "vscode";
 
-function removeTailwindClasses(): void {
+function removeTailwindClasses(classType: string): void {
   const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
   if (!editor) {
     vscode.window.showErrorMessage("No active editor found");
     return;
   }
 
-  const classList: string[] = [
-    "bg",
-    "text",
-    "leading",
-    "border",
-    "divide",
-    "ring",
-    "shadow",
-    "opacity",
-    "transition",
-    "duration",
-    "ease",
-    "delay",
-  ];
+  let classList: string[] = [];
+
+  switch (classType) {
+    case "typography":
+      classList = [
+        "font",
+        "text",
+        "whitespace",
+        "break",
+        "align",
+        "leading",
+        "list",
+        "placeholder",
+        "rte",
+        "tracking",
+      ];
+      break;
+    case "layout":
+      classList = [
+        "p",
+        "m",
+        "w",
+        "min",
+        "max",
+        "h",
+        "container",
+        "box",
+        "grid",
+        "flex",
+        "table",
+        "float",
+        "clear",
+        "absolute",
+        "fixed",
+        "inset",
+        "top",
+        "right",
+        "bottom",
+        "left",
+        "z",
+        "order",
+        "object",
+        "overflow",
+        "aspect",
+        "col",
+        "row",
+        "basis",
+        "grow",
+        "shrink",
+        "justify",
+        "content",
+        "items",
+        "align",
+        "place",
+        "gap",
+        "space",
+      ];
+      break;
+    case "styling":
+      classList = [
+        "bg",
+        "border",
+        "divide",
+        "ring",
+        "shadow",
+        "opacity",
+        "transition",
+        "duration",
+        "ease",
+        "delay",
+        "animate",
+      ];
+      break;
+    default:
+      removeAllClasses();
+      return;
+  }
 
   const regex: RegExp = new RegExp(
     `(?:^|\\s)(?:${classList.join("|")})-[\\w-]+`,
@@ -34,6 +97,8 @@ function removeTailwindClasses(): void {
   editor.edit((editBuilder: vscode.TextEditorEdit) => {
     editBuilder.replace(selection, newText);
   });
+
+  vscode.window.showInformationMessage(`Removed ${classType} classes`);
 }
 
 function removeClasses(text: string, regex: RegExp): string {
@@ -66,10 +131,19 @@ function removeAllClasses(): void {
 }
 
 export function activate(context: vscode.ExtensionContext): void {
-  let removeTailwindClassesFunction: vscode.Disposable =
-    vscode.commands.registerCommand(
-      "extension.removeTailwindClasses",
-      removeTailwindClasses
+  let removeTypographyClassesFunction: vscode.Disposable =
+    vscode.commands.registerCommand("extension.removeTypographyClasses", () =>
+      removeTailwindClasses("typography")
+    );
+
+  let removeLayoutClassesFunction: vscode.Disposable =
+    vscode.commands.registerCommand("extension.removeLayoutClasses", () =>
+      removeTailwindClasses("layout")
+    );
+
+  let removeStylingClassesFunction: vscode.Disposable =
+    vscode.commands.registerCommand("extension.removeStylingClasses", () =>
+      removeTailwindClasses("styling")
     );
 
   let removeAllClassesFunction: vscode.Disposable =
@@ -78,20 +152,38 @@ export function activate(context: vscode.ExtensionContext): void {
       removeAllClasses
     );
 
-  context.subscriptions.push(removeTailwindClassesFunction);
+  context.subscriptions.push(removeTypographyClassesFunction);
+  context.subscriptions.push(removeLayoutClassesFunction);
+  context.subscriptions.push(removeStylingClassesFunction);
   context.subscriptions.push(removeAllClassesFunction);
 
   let disposable = vscode.commands.registerCommand(
     "extension.downwind",
     async () => {
       const selectedOption = await vscode.window.showQuickPick(
-        ["Remove Tailwind Classes", "Remove All Classes"],
+        [
+          "Remove Typography Classes",
+          "Remove Layout Classes",
+          "Remove Styling Classes",
+          "Remove All Classes",
+        ],
         { canPickMany: false }
       );
-      if (selectedOption === "Remove Tailwind Classes") {
-        removeTailwindClasses();
-      } else if (selectedOption === "Remove All Classes") {
-        removeAllClasses();
+      switch (selectedOption) {
+        case "Remove Typography Classes":
+          removeTailwindClasses("typography");
+          break;
+        case "Remove Layout Classes":
+          removeTailwindClasses("layout");
+          break;
+        case "Remove Styling Classes":
+          removeTailwindClasses("styling");
+          break;
+        case "Remove All Classes":
+          removeAllClasses();
+          break;
+        default:
+          break;
       }
     }
   );
